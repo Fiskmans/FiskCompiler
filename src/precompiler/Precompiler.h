30220cad7532aaf50fe6d79d9539f5aab989a45e
Måns Zivkovic Andersen <mans.andersen@massive.se>
2026-04-22 17:26:33 +0200

    progress towards parsing pp directives

--- src/precompiler/Precompiler.h
+++ src/precompiler/Precompiler.h
@@ -6,12 +6,21 @@
 #include "precompiler/LineReader.h"
 #include "precompiler/LineJoiner.h"
 
+#include "pattern_matcher/PatternMatcher.h"
+#include "pattern_matcher/PatternBuilder.h"
+
 namespace fisk::precompiler
 {
 	
 
 	inline auto Precompile(std::string aFile)
 	{
+        std::ifstream cppBNFFile("data/cpp.bnf");
+		std::stringstream cppBNF;
+        cppBNF << cppBNFFile.rdbuf();
+
+		pattern_matcher::PatternMatcher matcher = pattern_matcher::PatternBuilder::FromBNF(cppBNF.str());
+
 		return SentinelRange(
 			ReIterator(
 				LineJoiner(
