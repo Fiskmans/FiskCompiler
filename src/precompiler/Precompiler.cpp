fc20839d55f2862d1090b3f9cad62dfd9a2e701a
Måns Zivkovic Andersen <mans.andersen@massive.se>
2026-04-23 17:14:47 +0200

    Progress towards parsing c++

--- src/precompiler/Precompiler.cpp
+++ src/precompiler/Precompiler.cpp
@@ -11,10 +11,10 @@ namespace fisk::precompiler
         std::ifstream cppBNFFile("data/cpp.bnf");
         std::stringstream cppBNF;
         cppBNF << cppBNFFile.rdbuf();
-
+        
         pattern_matcher::PatternMatcher matcher = pattern_matcher::PatternBuilder::FromBNF(cppBNF.str());
 
-        return 
+         return 
             SentinelRange(
                 ReIterator(
                     LineJoiner(std::make_shared<LineReader>(std::make_shared<std::ifstream>(aFile), aFile))));
